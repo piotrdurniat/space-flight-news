63ecd01302f4dc08b38165f8ff36c6f16a7b9f3b
Łukasz <mieczynskiukasz@gmail.com>
2022-11-16 00:18:50 +0100

    Screens with bottom navigation

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useEffect, useCallback } from 'react';
+import {Text, View, StyleSheet, Image, ImageBackground} from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import NavigationBar from "./components/NavigationBar";
+import {colors} from "./assets/colors/colors";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [fontsLoaded] = useFonts({
+        'Inter-Black': require('./assets/fonts/Oxygen-Bold.ttf'),
+    });
+    const image = { uri: "https://reactjs.org/logo-og.png" };
 
+    useEffect(() => {
+        async function prepare() {
+            await SplashScreen.preventAutoHideAsync();
+        }
+        prepare();
+    }, []);
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+               <NavigationBar style={styles.image}/>
+    );
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+    },
+    image: {
+        flex: 1,
+        width: null,
+        height: null,
+        backgroundColor: "#000000c0",
+
+    },
+    text: {
+        color: "white",
+        fontSize: 42,
+        lineHeight: 84,
+        fontWeight: "bold",
+        backgroundColor: "#000000c0",
+    }
 });
