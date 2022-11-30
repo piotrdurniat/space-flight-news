b6d4e0399f2cb49b62b2d32b50c01c5e69a9bd23
Piotr Durniat <piotr@durniat.com>
2022-11-30 11:33:43 +0000

    Resolve "Searchbar"

--- components/Reports/ReportsScreen.jsx
+++ components/Reports/ReportsScreen.jsx
@@ -1,20 +1,34 @@
+import { useMemo, useState } from "react";
 import { ScrollView } from "react-native";
 import { useQuery } from "react-query";
-import { Text } from "react-native-paper";
+import { Searchbar, Text } from "react-native-paper";
 import { Reports } from "../../api/Reports";
 import ReportCard from "./ReportCard";
 import theme from "../theme";
 
 export default function ReportsScreen() {
-  const reports = useQuery(["reports"], async () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const allReports = useQuery(["reports"], async () => {
     const { data } = await Reports.getAll();
     return data;
   });
 
+  const matchingReports = useQuery(["matching-reports", searchQuery], async () => {
+    const { data: reportsByTitle } = await Reports.searchByTitle(searchQuery);
+    const { data: reportsBySummary } = await Reports.searchBySummary(searchQuery);
+    return [...reportsBySummary, ...reportsByTitle];
+  });
+
+  const onChangeSearch = query => setSearchQuery(query);
+
+  const reports = useMemo(
+    () => (searchQuery !== "" ? matchingReports : allReports),
+    [matchingReports, allReports]
+  );
+
   return (
-    <ScrollView
-      style={{ padding: 8, backgroundColor: theme.colors.background }}
-    >
+    <ScrollView style={{ padding: 8, backgroundColor: theme.colors.background }}>
       <Text
         variant="displayMedium"
         style={{
@@ -25,14 +39,18 @@ export default function ReportsScreen() {
       >
         Reports
       </Text>
+      <Searchbar
+        style={{ marginBottom: 8 }}
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
       {reports.isError && <Text>Error fetching reports.</Text>}
 
       {reports.isLoading ? (
         <Text>Loading...</Text>
       ) : (
-        reports.data.map((report, index) => (
-          <ReportCard report={report} key={index} />
-        ))
+        reports.data.map((report, index) => <ReportCard report={report} key={index} />)
       )}
     </ScrollView>
   );
